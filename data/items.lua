cd0e1138c6ed64ddff43c04dfa1ea7e148b5f75f
Roberts Bauers <rrbauerss@gmail.com>
2023-08-31 16:11:19 +0300

    Update items.lua

--- data/items.lua
+++ data/items.lua
@@ -1,10 +1,8 @@
 -- Add this to ox_inventory/data/items.lua
+
 ['backpack'] = {
     label = 'Backpack',
     weight = 5000,
     stack = false,
     consume = 0,
-    server = {
-        export = 'void_backpacks.useBackpack'
-    }
 },
